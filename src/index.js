1c4c72942a016a9ce1b9eebb041ad08d4cb10258
Anand Patidar <139748329+anand9752@users.noreply.github.com>
2026-03-08 10:15:38 +0530

    hgcvhgh

--- src/index.js
+++ src/index.js
@@ -1,11 +1,1 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css"; // Ensure this file exists and is correctly linked
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
